8272aea94d5260fe4b3164f86d02eee8c021a14f
Olalekan Bamigboye <olalekanbamigboye732@gmail.com>
2025-08-14 14:28:42 +0100

    added two links

--- project-link-frontend/src/services/api.js
+++ project-link-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://project-link-itrn.onrender.com"
+    baseURL: "http://localhost:5000/api" || "https://project-link-itrn.onrender.com"
 
 })
 
