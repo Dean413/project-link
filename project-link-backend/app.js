c7806aa2f875dc232940e014b1218b4366dee836
Olalekan Bamigboye <olalekanbamigboye732@gmail.com>
2025-08-14 15:25:40 +0100

    updated cors again

--- project-link-backend/app.js
+++ project-link-backend/app.js
@@ -9,33 +9,14 @@ const projectRoutes = require("./routes/projectRoute");
 const errorHandler = require("./middleware/errorHandler"); 
 const cors = require("cors")
 
-const allowedOrigins = [
-  "http://localhost:5173", // local dev
-  "https://project-link-one.vercel.app/" // production frontend
-];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    // Allow requests with no origin (like mobile apps or curl)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
-}));
+app.use(cors({ origin: 'https://project-link-one.vercel.app' }));
 
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/clients", clientRoutes);
 app.use("/api/projects", projectRoutes);
-
-app.use((req, res) => {
-  res.status(404).json({ error: "Not Found" });
-});
 
 app.use(errorHandler);
 
