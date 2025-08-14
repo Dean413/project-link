66892c9adf15f96ac25ac8811dc071482f008a6f
Olalekan Bamigboye <olalekanbamigboye732@gmail.com>
2025-08-14 15:00:39 +0100

    changed base url

--- project-link-frontend/src/services/api.js
+++ project-link-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api" || "https://project-link-itrn.onrender.com"
+    baseURL: "https://project-link-itrn.onrender.com"
 
 })
 
