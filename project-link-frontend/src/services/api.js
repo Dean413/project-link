d3a294b7bbf8451d4602c56dff9e2e1aec347cac
Olalekan Bamigboye <olalekanbamigboye732@gmail.com>
2025-08-14 14:19:16 +0100

    link to backend

--- project-link-frontend/src/services/api.js
+++ project-link-frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://project-link-itrn.onrender.com"
+
 })
 
 API.interceptors.request.use(config => {
